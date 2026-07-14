0bfc46465ea29d4865ede4431ec1d1943eff7fb4
Ninacruz25 <casanova.gianina@gmail.com>
2026-07-14 17:46:35 -0600

    feat(login): add email and password inputs

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
